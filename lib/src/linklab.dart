ac1eb9ed59d1638f3cf29b44b3a7836f58ecec4a
nazarski <nazar.volitskii@gmail.com>
2025-03-31 23:48:21 +0300

    Reset to last working version

--- lib/src/linklab.dart
+++ lib/src/linklab.dart
@@ -1,5 +1,11 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
+import 'dart:developer' as developer;
+
+// Helper function for consistent logging
+void log(String message) {
+  developer.log(message, name: 'LinkLab');
+}
 
 class LinkLabData {
   final String id;
@@ -26,21 +32,32 @@ class LinkLabData {
     required this.domainType,
     required this.domain,
     this.parameters,
-  });
+  }) {
+    log('LinkLabData created: id=$id, fullLink=$fullLink');
+  }
 
   factory LinkLabData.fromMap(Map<dynamic, dynamic> map) {
+    log('LinkLabData.fromMap called with: ${map.toString()}');
+
     // Handle parameters coming from iOS SDK
     Map<String, String>? params;
     if (map['parameters'] != null) {
+      log('Processing parameters: ${map['parameters']}');
       if (map['parameters'] is Map) {
         params = Map<String, String>.from(
-          (map['parameters'] as Map).map((key, value) => 
-            MapEntry(key.toString(), value.toString()))
+            (map['parameters'] as Map).map((key, value) {
+              log('Parameter: $key = $value');
+              return MapEntry(key.toString(), value.toString());
+            })
         );
+      } else {
+        log('Warning: parameters is not a Map: ${map['parameters'].runtimeType}');
       }
+    } else {
+      log('No parameters provided');
     }
-    
-    return LinkLabData(
+
+    final data = LinkLabData(
       id: map['id'] as String,
       fullLink: map['fullLink'] as String,
       createdAt: map['createdAt'] as int,
@@ -53,10 +70,14 @@ class LinkLabData {
       domain: map['domain'] as String,
       parameters: params,
     );
+
+    log('LinkLabData.fromMap created object: id=${data.id}');
+    return data;
   }
 
   Map<String, dynamic> toMap() {
-    return {
+    log('Converting LinkLabData to Map: id=$id');
+    final map = {
       'id': id,
       'fullLink': fullLink,
       'createdAt': createdAt,
@@ -69,6 +90,8 @@ class LinkLabData {
       'domain': domain,
       'parameters': parameters,
     };
+    log('Converted to map: ${map.toString()}');
+    return map;
   }
 }
 
@@ -77,61 +100,139 @@ typedef LinkLabErrorCallback = void Function(String message, String? stackTrace)
 
 class LinkLab {
   static final LinkLab _instance = LinkLab._internal();
-  
-  factory LinkLab() => _instance;
-  
-  LinkLab._internal();
-  
+
+  factory LinkLab() {
+    log('LinkLab factory called');
+    return _instance;
+  }
+
+  LinkLab._internal() {
+    log('LinkLab._internal constructor called');
+  }
+
   final MethodChannel _channel = const MethodChannel('cc.linklab.flutter/linklab');
-  
-  final StreamController<LinkLabData> _dynamicLinkStream = 
-      StreamController<LinkLabData>.broadcast();
-  
+
+  final StreamController<LinkLabData> _dynamicLinkStream =
+  StreamController<LinkLabData>.broadcast();
+
   Stream<LinkLabData> get onLink => _dynamicLinkStream.stream;
-  
+
   LinkLabLinkCallback? _onLink;
   LinkLabErrorCallback? _onError;
-  
+
   Future<void> initialize() async {
+    log('LinkLab.initialize called');
     _channel.setMethodCallHandler(_handleMethod);
-    await _channel.invokeMethod('init');
+    log('Method call handler set');
+
+    try {
+      await _channel.invokeMethod('init');
+      log('LinkLab initialization successful');
+    } catch (e, stackTrace) {
+      log('Error during LinkLab initialization: $e');
+      log('Stack trace: $stackTrace');
+      rethrow;
+    }
   }
-  
+
   Future<dynamic> _handleMethod(MethodCall call) async {
+    log('Received method call: ${call.method}');
+    log('Arguments: ${call.arguments}');
+
     switch (call.method) {
       case 'onDynamicLinkReceived':
-        final data = LinkLabData.fromMap(call.arguments);
-        _dynamicLinkStream.add(data);
-        _onLink?.call(data);
+        log('Processing dynamic link');
+        try {
+          final data = LinkLabData.fromMap(call.arguments);
+          log('Successfully parsed LinkLabData: id=${data.id}');
+          _dynamicLinkStream.add(data);
+          log('Added to dynamic link stream');
+
+          if (_onLink != null) {
+            log('Calling onLink callback');
+            _onLink?.call(data);
+          } else {
+            log('No onLink callback registered');
+          }
+        } catch (e, stackTrace) {
+          log('Error processing dynamic link: $e');
+          log('Stack trace: $stackTrace');
+          _onError?.call('Error processing dynamic link: $e', stackTrace.toString());
+        }
         break;
+
       case 'onError':
+        log('Processing error from native side');
         final Map<dynamic, dynamic> args = call.arguments;
         final message = args['message'] as String;
         final stackTrace = args['stackTrace'] as String?;
+        log('Error from native: $message');
+        if (stackTrace != null) {
+          log('Native stack trace: $stackTrace');
+        }
         _onError?.call(message, stackTrace);
         break;
+
+      default:
+        log('Unknown method call: ${call.method}');
     }
   }
-  
+
   void setLinkListener(LinkLabLinkCallback onLink) {
+    log('Setting link listener');
     _onLink = onLink;
   }
-  
+
   void setErrorListener(LinkLabErrorCallback onError) {
+    log('Setting error listener');
     _onError = onError;
   }
-  
+
   Future<bool> isLinkLabLink(String link) async {
-    return await _channel.invokeMethod('isLinkLabLink', {'link': link}) ?? false;
+    log('isLinkLabLink called with: $link');
+    try {
+      final result = await _channel.invokeMethod('isLinkLabLink', {'link': link}) ?? false;
+      log('isLinkLabLink result: $result');
+      return result;
+    } catch (e, stackTrace) {
+      log('Error in isLinkLabLink: $e');
+      log('Stack trace: $stackTrace');
+      _onError?.call('Error checking if link is LinkLab link: $e', stackTrace.toString());
+      return false;
+    }
   }
-  
+
   Future<void> getDynamicLink(String shortLink) async {
-    await _channel.invokeMethod('getDynamicLink', {'shortLink': shortLink});
+    log('getDynamicLink called with: $shortLink');
+    try {
+      await _channel.invokeMethod('getDynamicLink', {'shortLink': shortLink});
+      log('getDynamicLink method invoked successfully');
+    } catch (e, stackTrace) {
+      log('Error in getDynamicLink: $e');
+      log('Stack trace: $stackTrace');
+      _onError?.call('Error getting dynamic link: $e', stackTrace.toString());
+      rethrow;
+    }
   }
-  
+
   Future<LinkLabData?> getInitialLink() async {
-    final data = await _channel.invokeMethod('getInitialLink');
-    if (data == null) return null;
-    return LinkLabData.fromMap(data);
+    log('getInitialLink called');
+    try {
+      final data = await _channel.invokeMethod('getInitialLink');
+      if (data == null) {
+        log('No initial link data available');
+        return null;
+      }
+
+      log('Initial link data received: $data');
+      final linkData = LinkLabData.fromMap(data);
+      log('Initial link parsed successfully: id=${linkData.id}');
+      return linkData;
+    } catch (e, stackTrace) {
+      log('Error in getInitialLink: $e');
+      log('Stack trace: $stackTrace');
+      _onError?.call('Error getting initial link: $e', stackTrace.toString());
+      return null;
+    }
   }
 }
